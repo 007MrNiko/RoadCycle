beb0bd6ce0062fe26332f800393a707a5783ca40
Mykola-Z!EB <niko050139892@gmail.com>
2019-10-04 22:08:50 +0300

    Add QTable

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
+#include <QPixmap>
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // resolution adaptation
+
     QApplication a(argc, argv);
+
+    //creating loading screen
+    QSplashScreen *loading_screen = new QSplashScreen;
+    loading_screen->setPixmap(QPixmap(":/BG/Data/Pictures/LoadingScreen.png"));
+    //loading_screen->setPixmap(QPixmap("./Data/Picture/LoadingScreen.png"));
+
+    loading_screen->show();
+
     MainWindow w;
-    w.show();
+
+    QTimer::singleShot(2500,loading_screen,SLOT(close()));
+    QTimer::singleShot(2500,&w,SLOT(show()));
+
     return a.exec();
 }
